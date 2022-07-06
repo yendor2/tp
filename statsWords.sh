3d8dc1a0289d57d3137ccf621a901aba682a347d
fabian <fabian@fabian.com>
2022-07-06 12:23:01 -0300

    Version final andando

--- statsWords.sh
+++ statsWords.sh
@@ -1,5 +1,6 @@
-  GNU nano 4.8                                                                                  statsWords.sh                                                                                              
 #!/bin/bash
+
+#Indicador estadístico de longitud de palabras (la más corta, la más larga y elpromedio de longitud).
 
 [ ! -f $1 ] && echo "Ingrese la ubicacion correcta del archivo de texto a analizar" && exit 1
 
@@ -10,9 +11,7 @@ max=0
 
 for palabra in $(cat $1)
 do
-	echo $palabra
 	tamanio=$(echo $palabra | tr -d [',''.'':'';'] | wc -L)
-	echo $tamanio
 	suma=$((tamanio+suma))
  	[ $tamanio -lt $min ] && min=$tamanio
 	[ $tamanio -gt $max ] && max=$tamanio
@@ -21,9 +20,6 @@ done
 cantidad=$(cat $1 | wc -w)
 promedio=$((suma/cantidad))
 
-echo cantidad $cantidad
-echo suma $suma
-echo promedio $promedio
 echo La palabra mas corta tiene $min caracteres
 echo La palabra mas larga tiene $max caracteres 
 echo El promedio de las longitudes es $promedio
